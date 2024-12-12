b99c491bc9d40e52e9221e437138b734a585858b
Aanika Mishra <a-mishra@onu.edu>
2024-12-12 18:44:25 -0500

    Finished 13B-2 and it works!

--- Laboratory 13B.cpp
+++ Laboratory 13B.cpp
@@ -12,12 +12,27 @@
 using namespace std;
 
 bool isLeapYear(int year);
+/*
+isLeapYear – determines whether a given year is a leap year
+under the Gregorian calendar.
+@param year the year; expected to be >= 1582
+@return true if year is a leap year; false otherwise
+*/
+
+int daysInMonth(int month, int year);
+/**
+daysInMonth – determines the number of days in a specified month
+@param month the month; expected to be in the range [1..12]
+@param year the year; expected to be >= 1582
+@return either 28, 29, 30, or 31, based on month and (leap) year
+*/
 
 int main()
 {
 	bool done = false;
+	int month;
 	int year;
-	while (!done)
+	/*while (!done) //Leap Year Loop
 	{
 		cout << "Please enter a year or Q to quit: ";
 		cin >> year;
@@ -41,6 +56,36 @@ int main()
 		}
 
 	}
+	*/
+	while (!done)
+	{
+		cout << "Please enter a month and a year or Q to quit: ";
+		cin >> month >> year;
+
+		if (cin.fail())
+		{
+			done = true;
+			break;
+		}
+		string monthName;
+		switch (month)
+		{
+		case 1: monthName = "January"; break;
+		case 2: monthName = "February"; break;
+		case 3: monthName = "March"; break;
+		case 4: monthName = "April"; break; 
+		case 5: monthName = "May"; break;
+		case 6: monthName = "June"; break;
+		case 7: monthName = "July"; break;
+		case 8: monthName = "August"; break;
+		case 9: monthName = "September"; break;
+		case 10: monthName = "October"; break;
+		case 11: monthName = "November"; break;
+		case 12: monthName = "December"; break;
+		}
+
+		cout << monthName << " " << year << " has " << daysInMonth(month, year) << " days." << endl;
+	}
 }
 
 bool isLeapYear(int year)
@@ -56,5 +101,31 @@ bool isLeapYear(int year)
 	else
 	{
 		return false;
+	}
+}
+int daysInMonth(int month, int year)
+{
+	if (month == 2 && isLeapYear(year) == false)
+	{
+		return 28;
+	}
+	else if (month == 2 && isLeapYear(year) == true)
+	{
+		return 29;
+	}
+	
+	switch (month)
+	{
+	case 1: return 31; break;
+	case 3: return 31; break;
+	case 4: return 30; break;
+	case 5: return 31; break;
+	case 6: return 30; break;
+	case 7: return 31; break;
+	case 8: return 31; break;
+	case 9: return 30; break;
+	case 10: return 31; break;
+	case 11: return 30; break;
+	case 12: return 31; break;
 	}
 }
